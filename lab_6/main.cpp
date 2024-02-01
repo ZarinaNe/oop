f620c40d062ae77e4719269db888fe50204d7bfe
ZarinaNe <93059396+ZarinaNe@users.noreply.github.com>
2024-02-01 19:41:37 +0300

    Delete lab_6 directory

--- lab_6/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Windows.h"
-#include "app_generator.h"
-#include "page_generator.h"
-int main() {
-	std::string os = "Windows";
-	buildTestPage(os);
-	//std::cout << appGenerator(os) << std::endl;
-	return 0;
-}
